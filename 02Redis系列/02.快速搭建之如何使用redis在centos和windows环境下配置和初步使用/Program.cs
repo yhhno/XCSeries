36eb63128ff3bf0765c02870b8ac9c8998742119
yhhno <516061849@qq.com>
2017-10-06 16:38:26 +0800

    02Redis系列 1.参照官方文档,在linux和windows平台上安装redis 2.要使用,先正确的评估,在正确的安装.

---
+++ 02Redis系列/02.快速搭建之如何使用redis在centos和windows环境下配置和初步使用/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.快速搭建之如何使用redis在centos和windows环境下配置和初步使用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
