f66cdcc1ef40dc959fbacfab4f358bae5b603415
yhhno <516061849@qq.com>
2017-10-06 18:34:40 +0800

    02Redis系列 1.既然运行起来了,就操作一下子呗,来个初体验 2.结合官网文档,要能完基本的操作,以及其他语言的操作.

---
+++ 02Redis系列/04.驱动尝试之下载Java的jedis和CSharp的StackExchange.Redis驱动连接redis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace _04.驱动尝试之下载Java的jedis和CSharp的StackExchange.Redis驱动连接redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //创建链接
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("192.168.23.151:6379");
+
+            //获取db
+            var database = redis.GetDatabase();
+
+            database.StringSet("username", "jack");
+
+            var str = database.StringGet("username");
+        }
+    }
+}
