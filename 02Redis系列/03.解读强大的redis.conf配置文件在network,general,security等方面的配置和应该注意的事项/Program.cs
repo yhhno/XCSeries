897f567941a2d8fceecbb9ed7fec0c56f5256fed
yhhno <516061849@qq.com>
2017-10-06 17:39:31 +0800

    02Redis系列 1.既然决定要使用,就先要先启动. 2.默认的启动方式,只能满足基本的需要, 3.个性的需要,就要修改配置文件了. 4.so,就需要知道配置文件有哪些配置项?都对应什么特性?来确定修改那个配置型,如何修改来达到目的? 5.要运用归纳,提高效率.

---
+++ 02Redis系列/03.解读强大的redis.conf配置文件在network,general,security等方面的配置和应该注意的事项/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.解读强大的redis.conf配置文件在network_general_security等方面的配置和应该注意的事项
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
