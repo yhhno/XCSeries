f2a64a2ce3911fd2ceff8fc3c08ee0a985096912
yhhno <516061849@qq.com>
2017-10-10 18:06:26 +0800

    02Redis系列 0.概念,原理,运用.对比,局限,有什么借鉴,了然于心? 1.发布订阅模式,原理,实现,命令,操作,局限 2.了然于胸=元素+元素关系+流程 3,无好处,无深入. 4.长连接有什么弊端? 为什么我不能发现? 5.知识的丰富,不足的纠正,模型的改进

---
+++ 02Redis系列/17.基于观察者模式的publish,subscribe发布订阅模式之Publish,PubSub,[P]Subscribe命令和源码分析/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.基于观察者模式的publish_subscribe发布订阅模式之Publish_PubSub__P_Subscribe命令和源码分析
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
