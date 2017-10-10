5a0b03c8dc357b7f4695ca88cae3f87e504cc827
yhhno <516061849@qq.com>
2017-10-10 16:01:26 +0800

    02Redis系列 0.概念,原理,运用.对比,局限,有什么借鉴,了然于心? 1.事务的结构,原理,命令. 2.干什么用呢? 3.What?how?Why? 与其它的对比 4,如何使用? 5.如何实现? 6.如何运用? 7.一路顺下来.

---
+++ 02Redis系列/16.transaction事务之如何通过exec,multi,watch在redis中进行简单的commit和rollback操作/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.transaction事务之如何通过exec_multi_watch在redis中进行简单的commit和rollback操作
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
