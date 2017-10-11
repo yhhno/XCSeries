f3bc0b412152cacf26c4ee9d39eb1e8ec568d797
yhhno <516061849@qq.com>
2017-10-11 18:30:31 +0800

    02Redis系列 0.概念,原理,运用.对比,局限,有什么借鉴,了然于心? 1.知识点.持久化的作用,流程,实现,两种持久化机制,每种持久化机制的流程和实现. 2.概念,刷盘,定时器,自动化保存,触发持久化机制,同步保存,异步保存,文本协议,缓冲区,阀值机制

---
+++ 02Redis系列/21.理解redis中的数据持久化原理之两大常用手段【AOF模式和RDB模式】介绍和分析/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.理解redis中的数据持久化原理之两大常用手段_AOF模式和RDB模式_介绍和分析
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
