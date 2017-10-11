2a8140d865f9f014ddd1196a47d85bca7c6e118f
yhhno <516061849@qq.com>
2017-10-11 11:51:29 +0800

    02Redis系列 0.概念,原理,运用.对比,局限,有什么借鉴,了然于心? 1.知识点: 优化方式,批量提交 2.概念:数据初始化,优化谁提供的,网络卡死,超时,多命令,单命令,pipeline(猜想如何实现的) 3.流程:不仅是系统流程,功能流程,还有命令流程 4.技巧:对比+猜想+模型 5.思维:填鸭式介绍,也填鸭式思考,思考是还浅尝辄止

---
+++ 02Redis系列/19.batchcommit性能优化之Pipeline管道模式介绍之如何使用使用CSharp和JAVA实现及通过Wireshark监视/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.batchcommit性能优化之Pipeline管道模式介绍之如何使用使用CSharp和JAVA实现及通过Wireshark监视
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
