daccf97c36d840e7161e97955db472f56f81edfe
yhhno <516061849@qq.com>
2017-10-17 15:07:42 +0800

    03Mongodb系列 1.shell的认识 2.编程环境的认识 3.交互的认识 4.获取帮助的认识 5.使用工具的评价

---
+++ 03MongoDb系列/05.mongodb shell使用loadjs与mongodb高速交互分析/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.mongodb_shell使用loadjs与mongodb高速交互分析
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
