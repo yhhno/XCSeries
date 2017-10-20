6dbdf05bdb3ed00ed7c989bb1e0e4dc2110a493b
yhhno <516061849@qq.com>
2017-10-20 20:58:49 +0800

    03Mongodb系列 1.spareindex, 奇葩的, 侧重点肯定不一样.关注点要换 2.unique index 唯一索引 3.TTL index 过期索引. 有特性的,关注点侧重的地方 4.哈哈

---
+++ 03MongoDb系列/18.mongodb索引之使用sparseIndex及ttlIndex及我们生产二次营销场景分析/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.mongodb索引之使用sparseIndex及ttlIndex及我们生产二次营销场景分析
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
