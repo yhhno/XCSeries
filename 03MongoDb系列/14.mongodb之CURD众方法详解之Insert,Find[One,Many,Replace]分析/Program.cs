68b88917a3e60c426e1cc2f2ec571543adfb34d0
yhhno <516061849@qq.com>
2017-10-20 14:52:02 +0800

    03Mongodb系列 1.docment的curd,概念,源码,使用,运用场景.都要有概念 2.遇到问题就分析,遇到新的就吸收,不用想太多. 3.做到不管新的旧的,都要心中有数,知道如何做,不能懵逼, 4.文档多起来,复杂起来,此时应该大体有个概念即可

---
+++ 03MongoDb系列/14.mongodb之CURD众方法详解之Insert,Find[One,Many,Replace]分析/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.mongodb之CURD众方法详解之Insert_Find_One_Many_Replace_分析
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
