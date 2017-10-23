c84b5258f58695481f0620516c4209286a8eaea9
yhhno <516061849@qq.com>
2017-10-23 17:51:30 +0800

    03Mongodb系列 1.sharding的特点,实现了负载. 和其他两种集群的区别,和redis的sharding的区别. 2.高可用 冗余,负载 3.Mongodb中操作的是docment 4.sharing架构时怎样的? 如何工作的? 之间如何相互工作的? 5. 操作步骤       1.开启一个配置好配置文件的config实例       2.开启一配置好配置文件的额mongos实例,命令不一样.此实例作用不一样.      3.开启多个sharding实例      4.在mongos的shell中将sharding实例添加到集群的sharding中,在db上开启分片功能,在db的collection上开启分片功能,设置shard key,      5.在mongos上的shell添加数据      6.为什么在mongos上操作呢? 集群的设置内容都在config上.      7.文档必须看,不然欠缺常识

---
+++ 03MongoDb系列/28.mongodb分布式部署之mongodb Sharding分片终极之海量数据部署/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28.mongodb分布式部署之mongodb_Sharding分片终极之海量数据部署
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
