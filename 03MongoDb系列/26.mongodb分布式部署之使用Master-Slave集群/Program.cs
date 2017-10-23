9bd15aefb7c49353593d58684468fb59403c0616
yhhno <516061849@qq.com>
2017-10-23 14:04:21 +0800

    03Mongodb系列 1.分布式部署的三种形式：master-slave，replica set，sharding 2.主从的概念，流程，工作机制，配置启动，缺陷（有延迟，不能自动故障转移） 3.如何同步的呢？

---
+++ 03MongoDb系列/26.mongodb分布式部署之使用Master-Slave集群/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26.mongodb分布式部署之使用Master_Slave集群
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
