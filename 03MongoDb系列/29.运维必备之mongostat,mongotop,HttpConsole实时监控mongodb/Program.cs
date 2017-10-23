39414e65e21cf7c7ad58bffaeb82a484d6215cf6
yhhno <516061849@qq.com>
2017-10-23 18:38:09 +0800

    03mongodb系列 1.监控 和性能 1.mongodb本身提供的工具,mongodb自带的命令,要会使用 2.第三方的 3,要多监控和性能有个基本的认识, 之前都不知道监控和性能是什么? 如何体现? 如何判断? 4.文档要看? 不能说是觉得简单,就不重视, 这是坏的行为,很大可能会养成坏的习惯.

---
+++ 03MongoDb系列/29.运维必备之mongostat,mongotop,HttpConsole实时监控mongodb/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29.运维必备之mongostat_mongotop_HttpConsole实时监控mongodb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
