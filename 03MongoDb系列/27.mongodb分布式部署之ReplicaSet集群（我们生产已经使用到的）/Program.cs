67c90949ce6e0d1678ed5643d12e826c47097137
yhhno <516061849@qq.com>
2017-10-23 15:18:34 +0800

    03Mongodb犀利 1.副本集的特点 p2p  cap,仲裁者 2.副本集的工作机制, 是否有概念? 3.副本集的流程, 是否有概念? 4.副本集的配置     1,启动实例,配置文件中配置集群名称     2.集群实例化     3.此时可以查看集群的概况     4.再启动一个配置集群名的实例.     5.通过命令,把实例添加到集群中,     6也可以添加一个仲裁者. 6.先抓脉络,形成整体上和流程上的概念. 做到心中有数,(关注点要准确),,然后再深挖细节.

---
+++ 03MongoDb系列/27.mongodb分布式部署之ReplicaSet集群（我们生产已经使用到的）/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27.mongodb分布式部署之ReplicaSet集群_我们生产已经使用到的_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
