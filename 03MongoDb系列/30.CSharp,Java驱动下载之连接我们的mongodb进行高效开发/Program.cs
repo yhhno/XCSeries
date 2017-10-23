7caef427444b5bd1512640e71d8ea8a97a09f6f5
yhhno <516061849@qq.com>
2017-10-23 19:32:15 +0800

    03mongodb系列 1.使用驱动来操作mongodb 2.新的场景来了,是否能面对自如呢?

---
+++ 03MongoDb系列/30.CSharp,Java驱动下载之连接我们的mongodb进行高效开发/Program.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30.CSharp_Java驱动下载之连接我们的mongodb进行高效开发
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var mydb = client.GetDatabase("ctrip");
+            var mycollection = mydb.GetCollection<Test>("plane");
+            //
+            mycollection.InsertOne(new Test()
+            {
+                Name = "jack"
+            });
+            ExpressionFilterDefinition<Test> expression = new ExpressionFilterDefinition<Test>(i => i.Name == "jack");
+            var items = mycollection.Find<Test>(expression).ToList();
+        }
+    }
+
+    public class  Test
+    {
+        public string Name { get; set; }
+    }
+}
