4a3a75c46c5c7e6f60d475d178a21a1d29ad3490
yhhno <516061849@qq.com>
2017-10-24 17:46:11 +0800

    09多线程系列 1.对thread有了基本的认识，空间和时间的消耗，性能的提升，以后使用的可是有顾虑的。 2.空间上的开销      1,thread的内核数据结构      2.thread 环境块      3.用户模式堆栈      4.内核模式堆栈 3时间上的开销      1.进程启动时的加载和thread的开启及销毁       2.时间片的切换 4.操作系统的概念,内核数据结构,osid,context,时间片切换,cpu调度,thread本地存储,winbg用户模式堆栈空间,内核模式堆栈空间,用户模式和内和模式的关系, win32函数,进程的启动,进程与线程的关系,应用程序域,sharding程序域, gac,标志位,逻辑处理器,并发执行 ,thread休眠, 5.得到thrad的使用判断依据

---
+++ 09多线程系列/01.Thread基础之从windbg角度理解你必须知道的时间和空间上的开销/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01.Thread基础之从windbg角度理解你必须知道的时间和空间上的开销
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(() => { }));
+            t.Start();
+            Console.ReadKey();
+        }
+    }
+}
