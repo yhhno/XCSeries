ae8d76926f10e219ebedbe66f706466c81baef12
yhhno <516061849@qq.com>
2017-10-26 16:19:57 +0800

    09多线程系列 1.AllocateDataSlot和ThreadStatic和ThreadLocal 2.如何理解线程可见于线程不可见 3.如何理解给线程分配一个数据槽 4.槽位,共享变量,可见性,程序集内,锁,主线程,工作线程,主线程与工作线程的区别,线程可见性,

---
+++ 09多线程系列/03.Thread静态方法介绍之认识三大TLS[线程本地存储]操作ThreadStatic和AllocateDataSlot等/program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _03.Thread静态方法介绍之认识三大TLS_线程本地存储_操作ThreadStatic和AllocateDataSlot等
+{
+    class Program
+    {
+        #region AllocateNamedDataSlot
+        //static void Main(string[] args)//主线程入口
+        //{
+        //    //演示下t1和t2各有一个变量
+        //    //首先定义一个槽位,也就是说在所有的线程上都分配了槽位,但槽位不一定都有数据
+        //    //槽位中塞入一个值helloworld
+        //    //现在有一个工作线程.
+        //    //也就是说变量是槽位的的值,两个线程分别为工作线程和主线程.
+        //    //我设置的槽位大家都是有的,但我在主线程上setdata,工作线程是获取不到的,同样的道理,我在工作线程中setdata,主线程也是获取不到的.
+        //    //我们也可以分配未命名的槽位,
+        //    var slot = Thread.AllocateNamedDataSlot("username");
+        //    //在主线程上设置槽位,也就是helloworld只能被主线程读取,其他线程无法读取
+        //    Thread.SetData(slot, "HelloWorld");
+
+        //    var t = new Thread(() => {
+        //        //工作线程获取不槽位的值. 这也就说明了演示下t1和t2各有一个变量,
+        //        //也就是说如果在工作线程中设置槽位的值,主线程也是获取不到的.
+        //        var obj = Thread.GetData(slot);
+        //        Console.WriteLine("当前工作线程:{0}", obj);
+        //    });
+        //    t.Start();
+
+        //    var obj2 = Thread.GetData(slot);
+        //    Console.WriteLine("主线程:{0}", obj2);
+
+        //    Console.Read();
+        //}
+        #endregion
+
+        #region ThreadStatic
+        //指示静态字段的值对于每个线程都是唯一的,也就是说程序集可见
+        //[ThreadStatic]
+        //static string username = string.Empty;
+        //static void Main(string[] args)//主线程入口
+        //{
+        //    username = "helloworld";
+        //    var t = new Thread(() =>
+        //    {
+
+        //        Console.WriteLine("当前工作线程:{0}", username);
+        //    });
+        //    t.Start();
+
+
+        //    Console.WriteLine("主线程:{0}", username);
+
+        //    Console.Read();
+        //}
+        #endregion
+
+
+
+       
+        static void Main(string[] args)//主线程入口
+        {
+            //提供数据的线程本地存储.
+            ThreadLocal<string> local = new ThreadLocal<string>();
+            local.Value = "HelloWorld";
+            var t = new Thread(() =>
+            {
+              
+                Console.WriteLine("当前工作线程:{0}", local.Value);
+                Thread.Sleep(10000000);
+            });
+            t.Start();
+
+
+            Console.WriteLine("主线程:{0}", local.Value);
+
+            Console.Read();
+        }
+
+
+    }
+}
