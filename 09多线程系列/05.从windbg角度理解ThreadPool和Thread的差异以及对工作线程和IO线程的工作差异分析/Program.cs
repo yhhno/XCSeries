30801a47af25d4a05e8b4ce3fb49e195ca5729fc
yhhno <516061849@qq.com>
2017-10-27 18:16:17 +0800

    09多线程系列 1.thread与threadPool的区别? 如何体现?写个例子,如何查看原因,?windbg, 2.Threadpool的机制原理 3.Thread的回收 4.cllr层面的thread,thread的专有性和thread的租用,参数为委托时,object类型包括委托,回调函数,windbg角度,thread的出生死亡,线程的回收,终结器,1m空间,线程默认初始化,工作线程,IO线程,8个逻辑处理器并行处理, 5.总结:各有千秋, thread自由度,threadpool性能. 6.要总结改善学习方法.

---
+++ 09多线程系列/05.从windbg角度理解ThreadPool和Thread的差异以及对工作线程和IO线程的工作差异分析/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _05.从windbg角度理解ThreadPool和Thread的差异以及对工作线程和IO线程的工作差异分析
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ThreadPool.QueueUserWorkItem((obj) => {
+            //    Console.WriteLine("我是工作线程:{0},context:{1}",Thread.CurrentThread.ManagedThreadId,obj);
+            //},"HelloWorld");
+            //Console.WriteLine("主线程:{0}",Thread.CurrentThread.ManagedThreadId);
+            //Console.Read();
+
+
+            //ThreadPool.QueueUserWorkItem((obj) => {
+            //    var func = obj as Func<string>;
+            //    Console.WriteLine("我是工作线程:{0},context:{1}", Thread.CurrentThread.ManagedThreadId, func());
+            //}, new Func<string>(()=> { return "HelloWorld"; }));
+            //Console.WriteLine("主线程:{0}", Thread.CurrentThread.ManagedThreadId);
+            //Console.Read();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Thread thread = new Thread(() => {
+            //        for (int j = 0; j < 10; j++)
+            //        {
+            //            Console.WriteLine("work:{0},tid:{1}",i,Thread.CurrentThread.m);
+            //        }
+            //    });
+            //    thread.Name = "main" + i;
+            //    thread.Start();
+            //}
+            //Console.Read();
+
+
+
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                ThreadPool.QueueUserWorkItem((obj) => {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        Console.WriteLine("work:{0},tid:{1}", i, Thread.CurrentThread.ManagedThreadId);
+                    }
+                });
+
+                //此时不可以设置线程名称,此时线程是租用的,不是专有的,专有的话我们可以做各种操作.线程池的线程不属于你,你只是租用而已
+                //thread.Name = "main" + i;
+                //thread.Start();
+            }
+            Console.Read();
+        }
+    }
+}
