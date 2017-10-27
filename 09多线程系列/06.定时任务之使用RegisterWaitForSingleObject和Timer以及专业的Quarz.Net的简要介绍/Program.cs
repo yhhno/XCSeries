369df278362aaae867cccbadc45ec2761ba66cdc
yhhno <516061849@qq.com>
2017-10-28 00:53:57 +0800

    09多线程系列 1.定时器实现的两种方式ThreadPool和Time 以及while try finally 语句 2.TreadPool如何实现定时器,Windbg查看底层 3.Timer如何实现定时器,ILSpy和windbg查看底层 4.定时器,mutex互斥锁,超时,等待waithandle,定时,锁机制,事件,信号量,回调函数,重置计时器,延期执行, 5.quarz.net

---
+++ 09多线程系列/06.定时任务之使用RegisterWaitForSingleObject和Timer以及专业的Quarz.Net的简要介绍/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _06.定时任务之使用RegisterWaitForSingleObject和Timer以及专业的Quarz.Net的简要介绍
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("main time:{0}", DateTime.Now);
+            //ThreadPool.RegisterWaitForSingleObject(new AutoResetEvent(false), new WaitOrTimerCallback((obj, b) => {
+
+            //    ////做逻辑判断，判断是否在否以时刻执行。。。
+            //    Console.WriteLine("obj:{0},tid:{1},time:{2}", obj, Thread.CurrentThread.ManagedThreadId, DateTime.Now);
+            //}), "helloworld", 1000, false);
+
+
+            //Console.Read();
+
+
+
+            Timer timer = new Timer(new TimerCallback((obj) => {
+                ////做逻辑判断，判断是否在否以时刻执行。。。
+                Console.WriteLine("obj:{0},tid:{1},time:{2}", obj, Thread.CurrentThread.ManagedThreadId, DateTime.Now);
+            }), "HelloWorld", 1000, 1000);
+
+           
+           
+
+            Console.Read();
+        }
+    }
+}
