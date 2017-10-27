b35d70b17556fcda37fbff990f3c8803cab971b3
yhhno <516061849@qq.com>
2017-10-28 02:15:22 +0800

    09多线程系列 1.Task是什么? 异步方法,为什么会出现? 与Thread和ThreadPool的异同? 2.Task的启动方式? 3.Task的底层源码,TaskScheduler. 4.Task可以返回值,不仅仅做thread的延续，阻塞，取消，超时等等功能 5.程序集内,如何做优化(以一些东西为底层),同步执行,阻塞执行,异步执行,如何理解基于

---
+++ 09多线程系列/07.Task和Task[T]之启动任务的三大方式Run,RunSynchronously,Start,StartNew以及内部代码异同点解析/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _07.Task和Task_T_之启动任务的三大方式Run_RunSynchronously_Start_StartNew以及内部代码异同点解析
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////实例化方式启动 异步方法
+            //Task task = new Task(() =>
+            //{
+            //    Console.WriteLine("我是工作线程:tid={0}", Thread.CurrentThread.ManagedThreadId);
+            //});
+            //task.Start();
+
+
+            //Task.Factory方式启动,类似于ThreadPool  异步方法
+            //var task = Task.Factory.StartNew(() =>
+            //{
+            //    Console.WriteLine("我是工作线程:tid={0}", Thread.CurrentThread.ManagedThreadId);
+            //});
+
+            ////Task.Run方式启动   异步方法
+            //var task = Task.Run(() =>
+            //{
+            //    Console.WriteLine("我是工作线程:tid={0}", Thread.CurrentThread.ManagedThreadId);
+            //});
+
+
+            ////Task的启动,,同步方法,也就是阻塞  什么是同步执行呢? a方法执行完,才能执行b方法. 大家是同步的
+            //Task task = new Task(() =>
+            //{
+            //    Console.WriteLine("我是工作线程:tid={0}", Thread.CurrentThread.ManagedThreadId);
+            //});
+            //task.RunSynchronously();
+
+
+            //Task<TResult>
+            var task = new Task<string>(() =>
+            {
+                return "Helloworld";
+            });
+            task.Start();
+            var msg = task.Result;
+
+            Console.WriteLine(msg);
+
+
+            Console.Read();
+        }
+    }
+}
