01b9bfb718394b1ff008499e7862f4d5b918db47
yhhno <516061849@qq.com>
2017-11-02 18:57:19 +0800

    09多线程系列 1.Plinq的出现？实现方式？新增的扩展方法？AsOrdered，AsParallel，AsSequential，并行与串行的选择？ 2.Linq使用很广的。 3.大前提是并行，并行的大前提是task 4.集成查询语言，linq，扩展方法， select投影，静态类，未排序的样子，并行的结果，并行转串行，windbg ，ClrStack调用堆栈。编程模型,WithDegreeOfParallelism,WithCancellation,WithExecutionMode强制并行,缓冲区.

---
+++ 09多线程系列/14.PLinq【AsParallel,AsOrdered】并行编程和ParallelEnumerable之对任何集合做并行执行/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _14.PLinq_AsParallel_AsOrdered_并行编程和ParallelEnumerable之对任何集合做并行执行
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////将linq装换为Plinq
+            //var nums = Enumerable.Range(0, 10);//定义一个数组
+            //var query = from n in nums.AsParallel()//转换为并行
+            //            select new// select 投影
+            //            {
+            //                thread = Thread.CurrentThread.ManagedThreadId,
+            //                nums = n
+            //             };
+            //foreach (var item in query)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            ////使用AsOrdered()
+            //var nums = Enumerable.Range(0, 10).ToList();//为什么要toList//定义一个数组
+            //nums[0] = 10000;//为什么要toList
+            //var query = from n in nums.AsParallel().AsOrdered()//转换为并行
+            //            select new// select 投影
+            //            {
+            //                thread = GetThreadId(),//这里可以使用方法.
+            //                nums = n
+            //            };
+            //foreach (var item in query)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            CancellationTokenSource source = new CancellationTokenSource();
+            source.Cancel();
+            try
+            {
+                var nums = Enumerable.Range(0, 10).ToList();//为什么要toList//定义一个数组
+                nums[0] = 10000;//为什么要toList
+                var query = from n in nums.AsParallel().AsOrdered().WithDegreeOfParallelism(Environment.ProcessorCount)
+                                                                   .WithCancellation(source.Token)//以抛出异常的方式,停止
+                                                                   .WithExecutionMode(ParallelExecutionMode.ForceParallelism)//强制执行并行
+                                                                   .WithMergeOptions(ParallelMergeOptions.Default)
+                            select new// select 投影
+                            {
+                                thread = GetThreadId(),//这里可以使用方法.
+                                nums = n
+                            };
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (AggregateException  ex)
+            {
+
+                Console.WriteLine(ex.InnerExceptions[0].Message);
+            }
+
+
+            Console.Read();
+        }
+
+        static int GetThreadId()
+        {
+            return Thread.CurrentThread.ManagedThreadId;
+        }
+    }
+}
