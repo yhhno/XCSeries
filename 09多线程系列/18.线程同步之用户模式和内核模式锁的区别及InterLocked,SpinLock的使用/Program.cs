a0ff3df2128cbb83e50bef3562875cb62321e2e2
yhhno <516061849@qq.com>
2017-11-05 18:11:31 +0800

    09多线程系列 1.啥是锁,?啥是锁机制? 概念上咋理解? 实质上在理解?  都有那些锁? 2.锁的分类: 1用户模式锁,2内核模式锁,3混合锁 3.用户模式锁    1.易变结构, volatile关键字 =>共享变量的操作,类似同步的作用,不知道如何实现?    2.互锁结构, interlocked,=>多线程中的共享变量的原子操作    3.旋转锁, 4.多线程到底有什么不一样? 锁为什么要用? 锁要达到什么目的?共享变量啥意思?啥作用? 原子性操作?锁基元啥意思?

---
+++ 09多线程系列/18.线程同步之用户模式和内核模式锁的区别及InterLocked,SpinLock的使用/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _18.线程同步之用户模式和内核模式锁的区别及InterLocked_SpinLock的使用
+{
+    class Program
+    {
+        public static volatile bool isStop = false;//类似一种同步锁 
+        public static SpinLock spinLock;
+        static void Main(string[] args)
+        {
+
+            #region 易变结构
+
+
+
+            //var isStop = false;//共享变量
+            //var t = new Thread(() =>
+            //  {
+            //      var isSuccess = false;
+            //      while(!isStop)
+            //      {
+            //          isSuccess = !isSuccess;
+            //      }
+            //  });
+            //t.Start();
+            //Thread.Sleep(1000*1);
+            //isStop = true;
+            //t.Join();
+            //Console.WriteLine("主线程执行结束!");
+
+
+
+
+
+            //var isStop = false;//共享变量
+            //var t = new Thread(() =>
+            //{
+            //    var isSuccess = false;
+            //    while (!isStop)
+            //    {
+            //        Thread.MemoryBarrier();//类似一种同步锁 
+            //        isSuccess = !isSuccess;
+            //    }
+            //});
+            //t.Start();
+            //Thread.Sleep(1000 * 1);
+            //isStop = true;
+            //t.Join();
+            //Console.WriteLine("主线程执行结束!");
+
+
+
+
+
+
+            //var isStop = 0;//共享变量
+            //var t = new Thread(() =>
+            //{
+            //    var isSuccess = false;
+            //    while (isStop == 0)
+            //    {
+            //         //类似一种同步锁 
+            //        Thread.VolatileRead(ref isStop); //此方法没有object类型的参数.怎么办? 就卡壳了? 不能的?
+            //        isSuccess = !isSuccess;
+            //    }
+            //});
+            //t.Start();
+            //Thread.Sleep(1000 * 1);
+            //isStop = 1;
+            //t.Join();
+            //Console.WriteLine("主线程执行结束!");
+
+
+
+
+
+
+            ////var isStop = false;//共享变量
+            //var t = new Thread(() =>
+            //  {
+            //      var isSuccess = false;
+            //      while (!isStop)
+            //      {
+            //          isSuccess = !isSuccess;
+            //      }
+            //  });
+            //t.Start();
+            //Thread.Sleep(1000 * 1);
+            //isStop = true;
+            //t.Join();
+            //Console.WriteLine("主线程执行结束!");
+
+            #endregion
+
+            #region 互锁结构
+
+            var sum = 0;
+            Interlocked.Increment(ref sum); //sum++
+            Interlocked.Decrement(ref sum);//sum--
+            Interlocked.Add(ref sum, 11);//sum+value
+            Interlocked.Exchange(ref sum, 111);//sum=value
+            Interlocked.CompareExchange(ref sum, 222, 111);//sum==111的话 ,sum=222
+            #endregion
+
+
+            //5个task
+            for (int i = 0; i < 5; i++)
+            {
+                Task.Factory.StartNew(() =>
+                {
+                    Run();
+                });
+            }
+
+            Console.ReadLine();
+        }
+
+        static int nums = 0;
+        static void Run()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    var b = false;
+                    spinLock.Enter(ref b);//有共享的机制,能保证依次操作,顺序操作,不然在多线程中是无序执行的.
+                    Console.WriteLine(nums++);//为什么没有重复.
+                }
+                catch ( Exception  ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    spinLock.Exit();
+                }
+            }
+        }
+    }
+}
