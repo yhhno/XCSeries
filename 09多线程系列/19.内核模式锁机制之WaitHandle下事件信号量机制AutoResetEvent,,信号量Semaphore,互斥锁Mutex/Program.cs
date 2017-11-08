91c64d0145a5a76ba6399ac813fe14bd2927e9f6
yhhno <516061849@qq.com>
2017-11-08 17:26:21 +0800

    09多线程系列 1.锁机制的内核模式,弊端 2.事件锁  如何理解事件?     1.自动事件锁     2.手动事件锁 3.信号量 4.互斥锁 5.为毛都有一个waitone方法

---
+++ 09多线程系列/19.内核模式锁机制之WaitHandle下事件信号量机制AutoResetEvent,,信号量Semaphore,互斥锁Mutex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _19.内核模式锁机制之WaitHandle下事件信号量机制AutoResetEvent__信号量Semaphore_互斥锁Mutex
+{
+    class Program
+    {
+        static Mutex mutex = new Mutex();
+
+        static AutoResetEvent areLock = new AutoResetEvent(false);//初始状态为非终止状态,表示此时闸机有票
+        static void Main(string[] args)
+        {
+            areLock.WaitOne(); //塞一张火车票到闸机中，因为此时有票在闸机，所以我只能等待  =》 此时是mainthread来执行这个操作. 也就是阻塞线程
+
+            Console.WriteLine("火车票检验通过，可以通行");
+
+            areLock.Set();   //从闸机中取火车票   也就是停止阻塞
+            
+        }
+    }
+}
