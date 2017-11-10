98c9271613ccd7950816b0f30ab46588dafa620f
yhhno <516061849@qq.com>
2017-11-10 19:52:17 +0800

    09多线程系列 1.线程不安全的集合,也就是没有锁机制的集合 2 ConcurrentQueue 3 ConcurrentDictionary<TKey, TValue> 4 ConcurrentStack<T> 5 ConcurrentBag<T> 6.并发集合都有那些?各自是如何使用的? 概念上是怎样?各自是如何实现的? 实质上是怎样?源码也要有概念上的认识?粗粗的知道如何实现? 也算是如何设计一个并发的数据结构?

---
+++ 09多线程系列/23.四大并发集合类ConcurrentBag Dictionary,Queue,Stack介绍以及对Bag实现进行源码分析/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _23.四大并发集合类ConcurrentBag_Dictionary_Queue_Stack介绍以及对Bag实现进行源码分析
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            //int num = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    bag.Add(num++);
+
+            //    //Task.Factory.StartNew(() =>
+            //    //{
+            //    //    bag.Add(num++);
+            //    //    Console.WriteLine("tid:{0}", Thread.CurrentThread.ManagedThreadId);
+            //    //    var subresult = 0;
+            //    //    bag.TryTake(out subresult);
+            //    //    Console.WriteLine(subresult);
+            //    //});//wait方法的区别, 为什么线程id是一样的.
+
+            //}
+            //var result = 0;
+            //bag.TryTake(out result);//移除和返回一个对象
+            //Console.WriteLine("主tid:{0}", Thread.CurrentThread.ManagedThreadId);
+            //Console.WriteLine(result);
+            //Console.Read();
+
+
+
+            //ConcurrentStack<int> stack = new ConcurrentStack<int>();
+            //stack.Push(1);
+            //stack.Push(2);
+            //var result = 0;
+            //stack.TryPop(out result);
+            //Console.WriteLine(result);
+            //Console.Read();
+
+
+
+
+            //ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+            //queue.Enqueue(1);
+            //var result = 0;
+            //queue.TryDequeue(out result);
+            //Console.WriteLine(result);
+            //Console.Read();
+
+
+
+            ConcurrentDictionary<int, int> dic = new ConcurrentDictionary<int, int>();
+            dic.TryAdd(1, 10);
+            dic.ContainsKey(1);
+            foreach (var item in dic)
+            {
+                Console.WriteLine(item.Key + item.Value);
+            }
+            Console.Read();
+        }
+    }
+}
