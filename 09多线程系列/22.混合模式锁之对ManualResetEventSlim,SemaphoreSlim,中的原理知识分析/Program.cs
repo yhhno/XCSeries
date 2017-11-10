b3a6682ad64fa9a49a6efe389ebd6f56a680a52f
yhhno <516061849@qq.com>
2017-11-10 18:02:51 +0800

    09多线程系列 1.混合锁? 啥是混合锁? 概念上? 实质上都是咋回事,对比(混合锁肯定性能高) 2.SemaphoreSlim： 3.ManualResetEventSlim： 4.ReaderWriterLockSlim： 5..各自锁的实现? 和使用? 以及各自的特性和可能组合使用得到的特性

---
+++ 09多线程系列/22.混合模式锁之对ManualResetEventSlim,SemaphoreSlim,中的原理知识分析/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _22.混合模式锁之对ManualResetEventSlim_SemaphoreSlim_中的原理知识分析
+{
+    class Program
+    {
+        static ManualResetEventSlim slim = new ManualResetEventSlim(); //此时是合围转态,不可通过.                                                                  
+       static SemaphoreSlim sslim = new SemaphoreSlim(1, 10); //默认1个线程同时运行，最大10个
+
+
+
+        //static void Main(string[] args)
+        //{
+
+        //    Task task = new Task(() =>
+        //      {
+        //          Run();
+        //      });
+        //    task.Start();
+        //    Thread.Sleep(1000*3);
+        //    //能组合出什么样的特性
+        //    slim.Set();//如何释放也是个问题? 释放过程没逻辑推理过.  是由谁来释放?
+        //    Console.Read();
+        //}
+        //static  void Run()
+        //{
+        //    slim.Wait(); //是由谁来阻止?
+
+        //    Console.WriteLine("我执行了");
+        //}
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(() =>
+                {
+                    Run();
+                });
+            }
+
+            //某一个时刻，我像改变默认的并行线程个数，从默认的1 改成10
+            //能组合出什么样的特性? 前两秒逐个执行,2秒后全部执行.
+            System.Threading.Thread.Sleep(2000);
+            sslim.Release(10);
+
+            Console.Read();
+        }
+
+        static void Run()
+        {
+            sslim.Wait();
+
+            Thread.Sleep(1000 * 1);
+
+            Console.WriteLine("当前t1={0} 正在运行 {1}", Thread.CurrentThread.ManagedThreadId, DateTime.Now);
+
+            sslim.Release();
+        }
+    }
+}
+
