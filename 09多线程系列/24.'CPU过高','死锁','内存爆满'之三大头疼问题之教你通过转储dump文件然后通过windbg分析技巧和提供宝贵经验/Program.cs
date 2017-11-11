b2023354e099c9c5f6ffd4a8e56caf53f2777759
yhhno <516061849@qq.com>
2017-11-11 18:41:43 +0800

    09多线程系列 1.多线程是啥? 为啥?能解决啥? 如何运用? 运用中是啥样? 2.Thread Task 是啥? 异步是啥?  为啥要出现? 出现前是啥?    这些都是都是出现前的事务的补充,而不是替代,  历史的基础的都不理解, 咋能运用好 高级的呢? 3.cpu过高  为什么会出现? 如何解决? 4.死锁  为什么会出现? 如何解决? 5.内存暴涨  为什么会出现? 如何解决? 6.不能听到问题毫无头绪,看到新名字无动于衷,看到旧知识没概念,  你这是占了那一头呢? 7.不管啥不要怕,往前冲,有头脑的分析,逐步演绎,逐步修正.

---
+++ 09多线程系列/24.'CPU过高','死锁','内存爆满'之三大头疼问题之教你通过转储dump文件然后通过windbg分析技巧和提供宝贵经验/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _24._CPU过高___死锁___内存爆满_之三大头疼问题之教你通过转储dump文件然后通过windbg分析技巧和提供宝贵经验
+{
+    class Program
+    {
+        static StringBuilder sb = new StringBuilder();
+        static void Main(string[] args)
+        {
+            //Run();
+
+            //new Program().Run();
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                sb.Append("HelloWorld");
+            }
+
+            Console.Read();
+        }
+
+
+        ////cpu过高
+        //static void Run()
+        //{
+        //    //此时task长久被cpu占用,因为cpu一直要这个操作?
+        //    var task = Task.Factory.StartNew(() =>
+        //    {
+        //        var i = true;
+        //        //这个地方是一个非常复杂的逻辑,导致死循环.
+        //        while (true)
+        //        {
+        //            i = !i;
+        //        }
+        //    });
+        //}
+
+        ////死锁
+        //   void Run()
+        //{
+        //    //使用lock,引用下当前类
+        //    lock (this)//这里锁住了当前类,通过同步块实现的
+        //    {
+        //        var task = Task.Run(() =>
+        //        {
+        //            Console.WriteLine("-----Start------");
+        //            Thread.Sleep(1000);
+                    
+        //            Run2();//假死状态.Run2中,要获取当前类的锁, 但此时锁还没有释放(task执行完才会释放.).获取不到.,
+        //            Console.WriteLine("-----End------");
+        //        });//Task 执行完后,释放lock锁
+
+        //        task.Wait();//加上这句,防止lock先退出,  为什么会先退出呢? task的大前提是什么? 异步呀? thread的大前提也是异步
+        //    }
+        //}
+
+        // void Run2()
+        //{
+        //    lock (this)//使用lock,引用下当前类
+        //    {
+        //        Console.WriteLine("我是Run2");
+        //    }
+        //}
+
+
+
+
+    }
+}
