10998a8f5e001a0317b194840028ebbae3df8260
yhhno <516061849@qq.com>
2018-08-13 19:10:52 +0800

    wcf04
    问题1：学习了解一个东西时的细节：它是什么？ 由什么组成？如何运转的？ 达到清楚的认识的效果。
    问题2：之前看到这这张图，这都是啥呀，看不懂，就不想看啊，要是勉强硬看，也是浏览下文字，原因：这种类型的东西，之前没有学过，也没有遇到过类似的，一下子看这个抽象的东西，看不懂，二，或者说，是不是之前只是了解使用，没有深入到源码原理呢？ 三又或者是，确实比较难，在第一步就放弃了，四又或者是本身就没有自信去做这件事，又或者这步只是应付时间，而不是真正的想去学习，
    问题3：这个图是wcf整体的架构，那架构时什么呢？
    问题4：激活和承载如何实现的呢？
    问题3：即时窗口和监视窗口是啥，有啥用，编码和序列化（序列化实体为二进制，二进制反序列化为实体）
    问题3：知道概念，还有知道如何实现的，
    问题5：当看到一个东西支持什么的时候，应该怎么想？ 比如NetTcpBinding支持安全验证，
    问题6； fiddler网络传输监控  学习使用工具

--- 07WCF系列/04/04.Basic基础：梳理wcf分层式通讯架构图【M,encoding,transport】及通过wireshark和fiddler详细分析/Program.cs
+++ 07WCF系列/04/04.Basic基础：梳理wcf分层式通讯架构图【M,encoding,transport】及通过wireshark和fiddler详细分析/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using _04servicelib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,11 @@ namespace _04.Basic基础_梳理wcf分层式通讯架构图_M_encoding_transport
     {
         static void Main(string[] args)
         {
+            ServiceHost host = new ServiceHost(typeof(Service1));//host承载Service1
+            host.Open();
+            Console.WriteLine("wcf启动成功");
+
+            Console.Read();
         }
     }
 }
