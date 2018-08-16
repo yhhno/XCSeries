47f83324c6d84ce7b22072a7af46b95f065d4ff7
yhhno <516061849@qq.com>
2018-08-16 17:59:00 +0800

    wcf05
    问题2：学习一种元数据格式
    问题3：注意这些都是预定义，或者说有一套默认的，那么我们可以自定义呢。
    问题1：   [ServiceContract]和   [OperationContract]是抽象的东西，底层是以wsdl为基础的操作东西

---
+++ 07WCF系列/05/05servicelib/HomeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace _05wcfservice
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的类名“Service1”。
+    public class HomeService : IHomeService
+    {
+        public  void DoWork(string msg)
+        {
+            //这些是输出到server端的额，可以做些监控或者日志
+            var ip = Dns.GetHostAddresses(Dns.GetHostName()).FirstOrDefault(i => i.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToString();
+            var info = string.Format("当前 request有 server={0} 返回message={1}", ip, msg);
+            Console.WriteLine(info);
+        }
+
+      
+    }
+}
---
+++ 07WCF系列/05/05servicelib/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05wcfservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(HomeService));
+            host.Open();
+            Console.WriteLine("wcf启动成功！");
+            Console.Read();
+
+        }
+    }
+}
