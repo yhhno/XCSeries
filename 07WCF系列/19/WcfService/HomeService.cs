08d40eda07ad097d827d56f5d3609269ead6d472
yhhno <516061849@qq.com>
2018-09-13 18:19:42 +0800

    wcf 19
    问题1：调度是啥意思呢？
    问题2： 我们这里知道tcp的效率大于http的，
    问题3：察觉问题，遇到问题怎么办？
    问题4： 方案变了，流程走一遍
    问题5：画个图，举个例子，都可以帮助我们理解？ 那最好的理解方式是什么呢？
    问题6:如果我在思考这个问题，我可能就会就此卡住了，完全不知道怎么去做？ 想不到监控？=》这说明基础实力不够，需要去扩充
    问题7： 监控如何监控？它的逻辑过程是啥？ 重试机制。
    问题9：新兴的工具，都是具有一些更完善的api实现。 有很多特性，可以帮助我们实现更好的功能？ =》但是要我们理解特性和功能的主方向和逻辑过程
    问题9.1 网上很多 基于redis 基于mongodb实现的某某功能，看到如何去理解，自己如何去实现？
    问题9.1 很多的术语，名词，都要搞清楚他解决了什么问题？ 原理是什么？
    问题10：情况变了，常用的添加服务引用没有了，怎么办？

---
+++ 07WCF系列/19/WcfService/HomeService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.MsmqIntegration;
+using System.Text;
+
+namespace WcfService
+{
+    public class HomeService : IHomeService
+    {
+        public string DoWork(string msg)
+        {
+            var info = string.Format("datetime={0} msg={1}", DateTime.Now, msg);
+
+            Console.WriteLine(info);
+
+            return info;
+        }
+    }
+}
---
+++ 07WCF系列/19/WcfService/Program.cs
@@ -0,0 +1,37 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceModel;
+using System.ServiceModel.MsmqIntegration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(HomeService));
+
+            host.Open();
+
+            //将url塞入redis中
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("192.168.23.193:6379");
+
+            var database = redis.GetDatabase();
+
+            var url = host.Description.Endpoints[0].Address.ToString();
+
+            var b = database.SetAdd("url", url);
+
+            Console.WriteLine("url={0} 插入 redis的状态:{1}", url, b);
+
+            Console.WriteLine("wcf启动成功！");
+            Console.Read();
+        }
+    }
+}
