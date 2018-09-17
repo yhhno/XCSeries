ea0a67f6f9c019fb8353e0b22d6e75114afaa65a
yhhno <516061849@qq.com>
2018-09-17 15:18:44 +0800

    wcf23 终结
    问题1：总结一个坑，已经使用或者自制一些提供效率的工具，这两个概念
    问题1：问题的抛出很重要，这样来龙去才清楚，
    问题2:逻辑过程缺失  需要注意的是这个配置，不能随着服务引用到client端，
    问题2：网路抖动是啥问题？ 超时  带宽很低  超时啥意思？你会这个操作不能超过1min=》认真理解每一个概念
    问题3：怎么少了反编译 这个概念呢？
    问题3：识别要求，分析成出方案。=》它既想给java使用（遵循ws标准），又想呢给C#使用（走net 也就是二进制的）
    问题4:默认生成的有两个endpoint，这个说明了什么？ =》脑子活泛
    问题5：说明webconfig 服务契约，等终归是配置，不是真正的执行程序，他们这些是要传入到执行程序里的=》脉络清楚=》脉络清楚的速度，叫做天赋
    
    问题6：微软提供的工具包，sdk是这个意思呀， 可视化  svrstrceviewer.exe=>了解的层次，范围 尽可能全面，不能说，教什么就些什么 对吧。

---
+++ 07WCF系列/23/WcfService/HomeService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.MsmqIntegration;
+using System.Text;
+
+namespace WcfService
+{
+    public class HomeService : IHomeService
+    {
+        public string DoWork(string msg)
+        {
+            Console.WriteLine(msg);
+
+            return msg;
+        }
+    }
+}
