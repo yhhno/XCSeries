57a8bc1cf2e77792a963ccf2d6c4cbe914703726
yhhno <516061849@qq.com>
2018-09-20 16:46:53 +0800

    rabbitmq 06
    问题1：交换机交换机，我们要知道交互机到底指的是什么？  就是一个类嘛，=》缺少用脑子去想
    问题2：显式声明是什么意思？ 就是手工加个嘛， 加个就完了嘛，=》要存在逻辑过程=》缺少用脑子去想
    问题3:看看文档，看看官方案例
    问题4：直联直联 啥意思嘛。
    问题5：这个轮询是如何实现的，以及queue和consumer是如何工作的？=》缺少逻辑过程=》缺少用脑子去想
    问题3:逻辑过程=》consumer必须也要声明队列=》如果声明队列的时候，队列已存在，应该没有操作
    问题3.1之前的步骤都一样，连接rabbitmq=>不错嘛，学会总结了
    问题1：设计类的参考，  发布订阅， 推拉 匿名函数
    问题2: 学过的东西，心中有个概念，如架构图=》也说明了先有个概况的主要性
    问题4:特性导致的优化 多个consumer有效的分摊了我们cpu的计算压力
    问题5:画个图，容易理解，
    问题5.1 binding，binding我们要知道binding到底指的是什么？背后做了什么？ 老是看高层名字，雨里雾里的，不清楚，看不到本质=》用脑子去想，看到本质

---
+++ 04Rabbitmq系列/Consumer/Program.cs
@@ -0,0 +1,92 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 5
+            //ConnectionFactory connectionFactory = new ConnectionFactory
+            //{
+            //    HostName = "127.0.0.1",
+            //    UserName = "qqqqqq",
+            //    Password = "qqqqqq",
+            //    //其他的设置，用默认的，此处不设置，
+            //};
+
+            ////第一步：创建connection
+            //var connection = connectionFactory.CreateConnection();
+
+            ////第二步：创建一个channel
+            //var channel = connection.CreateModel();
+
+            ////第三步：申明交换机【因为rabbitmq已经有了自定义的ampq default exchange】
+
+            ////以上的三步算是连接rabbitmq，后面的步骤是具体操作
+            ////第四步：获取消息
+            //var result = channel.BasicGet("mytest", true);
+
+            //var msg = Encoding.UTF8.GetString(result.Body);//result中有好多东西，相当于message的存储，
+            #endregion
+
+            #region 6
+            ConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = "127.0.0.1",
+                UserName = "qqqqqq",
+                Password = "qqqqqq",
+                //其他的设置，用默认的，此处不设置，
+            };
+
+            //第一步：创建connection
+            var connection = connectionFactory.CreateConnection();
+
+            //第二步：创建一个channel
+            var channel = connection.CreateModel();
+
+            //第三步：申明交换机【因为rabbitmq已经有了自定义的ampq default exchange】  我们当然也可以自定义交换机  也就是显式的指定自定义的交换机，非隐式指定默认的交换机   consumer是直接读队列的，所以交换机可以不声明
+            //channel.ExchangeDeclare("myexchange", ExchangeType.Direct, true, false, null);//也就是说有个交换机对象，像direct只是个类型而已=>用脑子去想
+         
+
+
+            //以上的四步算是连接rabbitmq，后面的步骤是具体操作=》有这个认识是，心中有个架构图
+            //第四步：声明一个队列(queue) 此处的队列如果不声明的话，可能会有个坑，=》consumer是直接读队列的，如果先运行consumer的话，如果没有指定的队列存在会出错的
+            channel.QueueDeclare("mytest", true, false, false, null);
+
+
+            
+
+            //主动获取
+            //var result = channel.BasicGet("mytest", true);
+            //var msg = Encoding.UTF8.GetString(result.Body);//result中有好多东西，相当于message的存储，
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            //consumer.Received += Consumer_Received;
+            consumer.Received += (sender, e)=>//匿名函数   e相当于restult，但是少了没有指定从哪个队列去获取数据
+            {
+                //获取msg
+                var msg = Encoding.UTF8.GetString(e.Body);
+
+                //只是我们此处的消费是把message输出，当然可以做其他操作。=》用脑子去想
+                Console.WriteLine(string.Format("我对message：{0},进行了消费，消费方式其实输出到控制台，当然有其他的方式",msg));
+            };
+
+            channel.BasicConsume("mytest", false, consumer);
+
+            Console.Read();//把进程拦住。
+            #endregion
+        }
+
+        //private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
--- 04Rabbitmq系列/Publish/Program.cs
+++ 04Rabbitmq系列/Publish/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,79 @@ namespace Publish
     {
         static void Main(string[] args)
         {
+            #region 5
+            //ConnectionFactory connectionFactory = new ConnectionFactory
+            //{
+            //    HostName = "127.0.0.1",
+            //    UserName = "qqqqqq",
+            //    Password = "qqqqqq",
+            //    //其他的设置，用默认的，此处不设置，
+            //};
+            ////第一步：创建connection
+            //var connection = connectionFactory.CreateConnection();
+            ////第二步：创建一个channel
+            //var channel = connection.CreateModel();
+            ////第三步：申明交换机【因为rabbitmq已经有了自定义的ampq default exchange】
+
+            ////第四步：创建一个队列(queue)
+            //channel.QueueDeclare("mytest", true, false, false, null);
+
+            //var msg = Encoding.UTF8.GetBytes("你好"); //因为我们要生成byte数组
+
+            ////以上的四步算是连接rabbitmq，后面的步骤是具体操作
+            ////第五步：发布消息   发布消息都用basic前缀
+            ////channel.BasicPublish(string.Empty, "mytest", null, msg);
+            //channel.BasicPublish(string.Empty, routingKey: "mytest", basicProperties: null, body: msg);
+
+
+            //Console.Read();//拦住这个进程。
+            //               //为啥不释放，释放了ui中就看不到了
+            //               //using。。。。
+            //               //channel.Dispose();
+            //               //connection.Dispose();
+            #endregion
+
+            #region 6
+            ConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = "127.0.0.1",
+                UserName = "qqqqqq",
+                Password = "qqqqqq",
+                //其他的设置，用默认的，此处不设置，
+            };
+            //第一步：创建connection
+            var connection = connectionFactory.CreateConnection();
+            //第二步：创建一个channel
+            var channel = connection.CreateModel();
+            //第三步：申明交换机【因为rabbitmq已经有了自定义的ampq default exchange】 我们当然也可以自定义交换机  也就是显式的指定自定义的交换机，非隐式指定默认的交换机  publish是直接把消息推给交换机的，但交换机有默认的，所以交换机可以不声明
+            //channel.ExchangeDeclare("myexchange", ExchangeType.Direct, true, false, null);//也就是说有个交换机对象，像direct只是个类型而已=>用脑子去想
+
+            //第四步：创建一个队列(queue)
+            channel.QueueDeclare("mytest", true, false, false, null);
+
+            //如果是自定义exchange的话，需要把queue绑定到自定义的exchange上，容易出错，但如果是隐式的话，这些工作由rabbitmq默认完成了。
+            //也就是说在erlang的measia数据库中个，有一个路由表，有三个列 exchangename queuename routingkey ，bind就是向这张表插入一条数据，，供以后使用，
+            //channel.QueueBind("mytest", "myexchange", "mytest", null);//routingkey默认是队列名，那也可以自己定义=》可以自定义的这个特性，可以实现是什么功能呢
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                var msg = Encoding.UTF8.GetBytes(string.Format("{0}+{1}",i,"你好")); //因为我们要生成byte数组
+
+                //以上的四步算是连接rabbitmq，后面的步骤是具体操作  =》有这个认识是，心中有个架构图
+                //第五步：发布消息   发布消息都用basic前缀=》先推个交换机，后续工作，交换机完成，如把消息推到指定的队列中
+                //channel.BasicPublish(string.Empty, "mytest", null, msg);
+                channel.BasicPublish(string.Empty, routingKey: "mytest", basicProperties: null, body: msg);
+            }
+
+
+            Console.Read();//拦住这个进程。
+            //为啥不释放，释放了ui中就看不到了
+            //using。。。。
+            //channel.Dispose();
+            //connection.Dispose();
+            #endregion
+
         }
     }
 }
