bbcd7a7eb3af47da3aa0bd404f1e7882f20365b8
yhhno <516061849@qq.com>
2018-09-21 19:49:07 +0800

    rabbitmq09
    问题0：当时看到这里时，心里就充满了怕怕的，蒙的，不知道我自己要做什么了。
    问题1：一开始看到这段英文肯定是心里怕怕的，要做的就是战胜这种怕怕，去找个主方向，走一下逻辑过程，把它给了解透透的
    问题1.1 只有英文介绍，那英文阅读是不是很重要
    问题2：学习routingkey的时候，为啥没觉得它太单一了呢？
    问题2.1当然routingkey的值，可以是任何值，不能简单的认为是error
    问题1：新东西，新的逻辑过程，不能说经验主义，
    问题3:做个对比，
    问题3：新特性，新特性可以类比下之前的知识，这样好理解 ，可以和什么样的业务需求结合起来呢、
    问题6：routingkey和mulipe attribute 保存在哪里呢?=>保存在message实体上，说明有个message对象，包装了这些东西和msg
    问题7：但是我原本想是这样的，先在一个地方写多个值，然后在一个地方设置x-match=》错了，没有认真去看文档=》这个完全可以加个逻辑判断就可以了，先判断x-match的值，然后决定接下来的逻辑处理，=》文档看的不仔细呀，想想当时看到了，可以理解不，=》这个需要文档+哪个绑定时的参数类型共同去看才可以的。=》有可能看了，也不知道如何先手，不能气馁，更不能停止
    问题7.1经验主义哦=>也说明经验很少

--- 04Rabbitmq系列/Consumer/Program.cs
+++ 04Rabbitmq系列/Consumer/Program.cs
@@ -152,6 +152,81 @@ namespace Consumer
             #endregion
 
             #region 8
+           // ConnectionFactory connectionFactory = new ConnectionFactory
+           // {
+           //     HostName = "127.0.0.1",
+           //     UserName = "qqqqqq",
+           //     Password = "qqqqqq",
+           //     //其他的设置，用默认的，此处不设置，
+           // };
+
+           // //第一步：创建connection
+           // var connection = connectionFactory.CreateConnection();
+
+           // //第二步：创建一个channel
+           // var channel = connection.CreateModel();
+
+           // //第三步：申明交换机【因为rabbitmq已经有了自定义的ampq default exchange】  我们当然也可以自定义交换机  也就是显式的指定自定义的交换机，非隐式指定默认的交换机   consumer是直接读队列的，所以交换机可以不声明
+           // channel.ExchangeDeclare("myfanoutexchange", ExchangeType.Direct, true, false, null);//也就是说有个交换机对象，像direct只是个类型而已=>用脑子去想
+
+
+
+           // //以上的四步算是连接rabbitmq，后面的步骤是具体操作=》有这个认识是，心中有个架构图
+           // //第四步：声明一个队列(queue) 此处的队列如果不声明的话，可能会有个坑，=》consumer是直接读队列的，如果先运行consumer的话，如果没有指定的队列存在会出错的
+           // channel.QueueDeclare("myfanoutqueue1", true, false, false, null);
+           // channel.QueueDeclare("myfanoutqueue2", true, false, false, null);
+
+
+           // //如果是自定义exchange的话，需要把queue绑定到自定义的exchange上，容易出错，但如果是隐式的话，这些工作由rabbitmq默认完成了。
+           // //也就是说在erlang的measia数据库中个，有一个路由表，有三个列 exchangename queuename routingkey ，bind就是向这张表插入一条数据，，供以后使用，
+           // //此需求，必须实现自定义exchange， 默认的无法实现
+           // //也说明了，自定义exchange，更灵活，可以实现更强大的功能
+           // //此处有个问题：队列 exchange的声明，相互间的binding，应该是在管理端去完成，在publish端完成这个逻辑的绑定是不是不符合常理，也可以定义在消费端=》说明了无论publish consumer不仅仅是做publish和消费的工作，还是管理rabbitmq。=》主方向
+           // //当然这些东西，能不在代码中实现，就不要在代码中实现
+           // //潜在的一个问题：不要例子这样写，就认为全部都是这样=》找准主方向
+           // //channel.QueueBind("log_else", "myexchange", "debug", null);//routingkey默认是队列名，那也可以自己定义=》可以自定义的这个特性，可以实现是什么功能呢
+           // //channel.QueueBind("log_else", "myexchange", "info", null);
+           // //channel.QueueBind("log_else", "myexchange", "warning", null);
+           // //channel.QueueBind("log_error", "myexchange", "error", null);//一个consumer只消费一个queue，所以不关error=》当时我还想着如何在一个地方处理不同的情况呢？=》分析不清楚，=》逻辑过程没走，经验主义=>之前想的是一个consumer只处理一个queue，但此时只是定义，不是处理queue呀，所以说可以全部定义的，当然也可以在两个处理queue中分别去定义，绑定
+
+           // //////优化版本
+           // ////var attr = new string[]{ "debug", "info","warning" };
+           // ////for (int i = 0; i < attr.Length; i++)
+           // ////{
+           // ////    channel.QueueBind("log_error", "myexchange",attr[i], null);/routingkey默认是队列名，那也可以自己定义=》可以自定义的这个特性，可以实现是什么功能呢
+
+           // ////}
+           // ////channel.QueueBind("log_error", "myexchange", "error", null);//一个consumer只消费一个queue，所以不关error=》理解有问题
+
+           // //理解为什么定义两个队列，但consumer的话，就单独定义，比较好，消费那个，就定义那个，如此处consumer1消费myfanoutqueue1，就只定义myfanoutqueue1.
+           // channel.QueueBind("myfanoutqueue1", "myfanoutexchange", string.Empty, null);//要理解为什么没有routingkey
+           // channel.QueueBind("myfanoutqueue2", "myfanoutexchange", string.Empty, null);//要理解为什么没有routingkey
+
+
+           // //主动获取
+           // //var result = channel.BasicGet("mytest", true);
+           // //var msg = Encoding.UTF8.GetString(result.Body);//result中有好多东西，相当于message的存储，
+
+           // EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+           // //consumer.Received += Consumer_Received;
+           // consumer.Received += (sender, e) =>//匿名函数   e相当于restult，但是少了没有指定从哪个队列去获取数据
+           // {
+           //     //获取msg
+           //     var msg = Encoding.UTF8.GetString(e.Body);
+
+           //     //只是我们此处的消费是把message输出，当然可以做其他操作。=》用脑子去想
+           //     Console.WriteLine(string.Format("我对message：{0},进行了消费，消费方式其实输出到控制台，当然有其他的方式", msg));
+           // };
+
+           // //定义了两个队列，单个consumer消费其中的一个队列，此时就不可以点击两次exe，充当两个consumer了。=>拒绝不加思考的就点击两次exe
+           // channel.BasicConsume("myfanoutqueue1", false, consumer);
+
+
+           // Console.WriteLine("consumer端启动完成！！！");
+           //Console.Read();//把进程拦住。
+            #endregion
+
+            #region 9
             ConnectionFactory connectionFactory = new ConnectionFactory
             {
                 HostName = "127.0.0.1",
@@ -167,14 +242,14 @@ namespace Consumer
             var channel = connection.CreateModel();
 
             //第三步：申明交换机【因为rabbitmq已经有了自定义的ampq default exchange】  我们当然也可以自定义交换机  也就是显式的指定自定义的交换机，非隐式指定默认的交换机   consumer是直接读队列的，所以交换机可以不声明
-            channel.ExchangeDeclare("myfanoutexchange", ExchangeType.Direct, true, false, null);//也就是说有个交换机对象，像direct只是个类型而已=>用脑子去想
+            channel.ExchangeDeclare("myHeadersexchange", ExchangeType.Headers, true, false, null);//也就是说有个交换机对象，像direct只是个类型而已=>用脑子去想
 
 
 
             //以上的四步算是连接rabbitmq，后面的步骤是具体操作=》有这个认识是，心中有个架构图
             //第四步：声明一个队列(queue) 此处的队列如果不声明的话，可能会有个坑，=》consumer是直接读队列的，如果先运行consumer的话，如果没有指定的队列存在会出错的
-            channel.QueueDeclare("myfanoutqueue1", true, false, false, null);
-            channel.QueueDeclare("myfanoutqueue2", true, false, false, null);
+            channel.QueueDeclare("myHeaderqueue1", true, false, false, null);
+            channel.QueueDeclare("myHeaderqueue2", true, false, false, null);
 
 
             //如果是自定义exchange的话，需要把queue绑定到自定义的exchange上，容易出错，但如果是隐式的话，这些工作由rabbitmq默认完成了。
@@ -199,8 +274,18 @@ namespace Consumer
             ////channel.QueueBind("log_error", "myexchange", "error", null);//一个consumer只消费一个queue，所以不关error=》理解有问题
 
             //理解为什么定义两个队列，但consumer的话，就单独定义，比较好，消费那个，就定义那个，如此处consumer1消费myfanoutqueue1，就只定义myfanoutqueue1.
-            channel.QueueBind("myfanoutqueue1", "myfanoutexchange", string.Empty, null);//要理解为什么没有routingkey
-            channel.QueueBind("myfanoutqueue2", "myfanoutexchange", string.Empty, null);//要理解为什么没有routingkey
+            channel.QueueBind("myHeaderqueue1", "myHeadersexchange", string.Empty, new Dictionary<string, object>
+            {
+                { "x-match","any"},
+                { "username","jack"},
+                { "password","123456"}
+            });//要理解为什么没有routingkey,但是我原本想是这样的，先在一个地方写多个值，然后在一个地方设置x-match=》错了，没有认真去看文档=》这个完全可以加个逻辑判断就可以了，先判断x-match的值，然后决定接下来的逻辑处理，=》文档看的不仔细呀，想想当时看到了，可以理解不，=》这个需要文档+哪个绑定时的参数类型共同去看才可以的。=》有可能看了，也不知道如何先手，不能气馁，更不能停止
+            channel.QueueBind("myHeaderqueue2", "myHeadersexchange", string.Empty, new Dictionary<string, object>
+            {
+                { "x-match","all"},
+                { "username","jack"},
+                { "password","123456"}
+            });//要理解为什么没有routingkey,但是我原本想是这样的，先在一个地方写多个值，然后在一个地方设置x-match=》错了，没有认真去看文档=》这个完全可以加个逻辑判断就可以了，先判断x-match的值，然后决定接下来的逻辑处理，=》文档看的不仔细呀，想想当时看到了，可以理解不，=》这个需要文档+哪个绑定时的参数类型共同去看才可以的。=》有可能看了，也不知道如何先手，不能气馁，更不能停止
 
 
             //主动获取
@@ -219,11 +304,11 @@ namespace Consumer
             };
 
             //定义了两个队列，单个consumer消费其中的一个队列，此时就不可以点击两次exe，充当两个consumer了。=>拒绝不加思考的就点击两次exe
-            channel.BasicConsume("myfanoutqueue1", false, consumer);
+            channel.BasicConsume("myHeaderqueue1", false, consumer);
 
 
             Console.WriteLine("consumer端启动完成！！！");
-           Console.Read();//把进程拦住。
+            Console.Read();//把进程拦住。
             #endregion
         }
 
