474e425c0f5c31ec787d8a672dfd15050649cc92
DownUp <DownUp@DESKTOP-59TN5FJ>
2019-07-23 17:49:23 +0800

    part11

--- 04Rabbitmq系列/15.Message级别控制之金融系AMQP的消息确认机制[txSelec,TxComit]和发布确认[Ack,Reject]/Program.cs
+++ 04Rabbitmq系列/15.Message级别控制之金融系AMQP的消息确认机制[txSelec,TxComit]和发布确认[Ack,Reject]/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _15.Message级别控制之金融系AMQP的消息确认机制_txSelec_TxComit._._._和发布确认_Ack_Reject._._._
+namespace _15.Message级别控制之金融系AMQP的消息确认机制_txSelec_TxComit_和发布确认_Ack_Reject_
 {
     class Program
     {
--- 04Rabbitmq系列/15.Message级别控制之金融系AMQP的消息确认机制[txSelec,TxComit]和发布确认[Ack,Reject]/Properties/AssemblyInfo.cs
+++ 04Rabbitmq系列/15.Message级别控制之金融系AMQP的消息确认机制[txSelec,TxComit]和发布确认[Ack,Reject]/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("15.Message级别控制之金融系AMQP的消息确认机制[txSelec,TxComit...]和发布确认[Ack,Reject...]")]
+[assembly: AssemblyTitle("15.Message级别控制之金融系AMQP的消息确认机制[txSelec,TxComit]和发布确认[Ack,Reject]")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("15.Message级别控制之金融系AMQP的消息确认机制[txSelec,TxComit...]和发布确认[Ack,Reject...]")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("15.Message级别控制之金融系AMQP的消息确认机制[txSelec,TxComit]和发布确认[Ack,Reject]")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("c6d4da62-e8de-4d9d-8f5a-cd5ecf85a5d4")]
+[assembly: Guid("2fe53196-42a6-4c38-863f-9d4ffec55e38")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
